74910eef16fd7fa571d10af2260dc98122efd08c
Josue Peralta <jperal77@gmail.com>
2018-12-19 01:14:48 -0600

    clients can join a public room and clients are notified when a user enters/leaves the public room

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(helmet());
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const users = {};
 
 app.get('/', (req, res) => {
   res.send('Server running...');
@@ -13,7 +14,21 @@ app.get('/', (req, res) => {
 io.on('connection', socket => {
   console.log('a user connected');
 
+  socket.on('join public', username => {
+    socket.join('public', () => {
+      users[socket.id] = { username: username };
+
+      console.log(users[socket.id].username);
+      socket.to('public').emit('new user connected', username);
+    });
+  });
+
   socket.on('disconnect', () => {
+    if (users[socket.id]) {
+      socket.to('public').emit('user disconnected', users[socket.id].username);
+      delete users[socket.id];
+    }
+
     console.log('user disconnected');
   });
 });
